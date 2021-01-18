bf88a207e3188d38198427d14f342b609b746f1a
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-18 13:21:41 -1000

    todo list stretch

---
+++ src/Todos.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.css";
+
+/* Form:  Input, submit  */
+/*component to display todo list items */
+/* useState [] to hold todos with {id: <todos.id>, name: <todos.name> status: <complete/incomplete>} */
+/* way to delete todo items as well as to strike through*/
+
+const initialTodo = {
+  id: "",
+  description: "",
+  complete: false,
+};
+
+const initialTodoList = [
+  {
+    id: uuidv4(),
+    description: "Click to Toggle Done/ Undone",
+    complete: false,
+  },
+];
+const Todos = () => {
+  const [todos, setTodos] = useState(initialTodoList);
+  const [todo, setTodo] = useState(initialTodo);
+
+  const handleChange = (e) => {
+    setTodo({ ...todo, description: e.target.value });
+  };
+
+  console.log(todo.description);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(uuidv4());
+
+    setTodos([...todos, { ...todo, id: uuidv4() }]);
+    setTodo(initialTodo);
+    console.log({ todos });
+  };
+
+  const handleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <div className="card container d-flex flex-column align-content-center justify-content-center">
+      <div
+        className="d-flex flex-column justify-content-center
+       align-content-center todo-page"
+      >
+        <h3 className="display-4" style={{ padding: "2rem" }}>
+          Tony's Todo list
+        </h3>
+        <div className="d-flex justify-content-center align-content-center">
+          <form
+            className="todo-form d-flex align-content-center"
+            onSubmit={submitHandler}
+          >
+            <textarea
+              rows={1}
+              className={"todo-form"}
+              name="Todo"
+              value={todo.description}
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="todo-submit-button"
+              onSubmit={submitHandler}
+            >
+              Add Todo
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div
+        className="todo-list d-flex flex-column"
+        style={{ padding: "3rem", textAlign: "center", marginTop: "2rem" }}
+      >
+        {todos.map((todo) => {
+          return (
+            <div
+              onClick={() => handleCompleted(todo.id)}
+              key={todo.id}
+              style={{
+                border: "2px, solid, black",
+                padding: "1rem 5rem",
+                margin: "1rem 0",
+                textDecoration: todo.complete && "line-through",
+                backgroundColor: todo.complete && "black",
+                color: todo.complete && "#444",
+                opacity: todo.complete && "0.4",
+              }}
+            >
+              <ul>
+                <li
+                  style={{
+                    textShadow: "none",
+                    textAlign: "center",
+                  }}
+                >
+                  {todo.description}
+                </li>
+              </ul>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
 
-import Counter from './components/Counter';
-import Input from './components/Input';
-import Moods from './components/Moods';
-import Spinner from './components/Spinner';
-import Squares from './components/Squares';
-import Programmers from './components/Programmers';
+import Counter from "./components/Counter";
+import Input from "./components/Input";
+import Moods from "./components/Moods";
+import Spinner from "./components/Spinner";
+import Squares from "./components/Squares";
+import Programmers from "./components/Programmers";
+import Todos from "./Todos.js";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 render(
   <>
-    <Counter />
+    {/* <Counter />
     <Moods />
     <Spinner />
     <Input />
     <Squares />
-    <Programmers />
-  </>
-  ,
-  document.querySelector('#root')
+    <Programmers /> */}
+    <div>
+      <Todos />
+    </div>
+  </>,
+  document.querySelector("#root")
 );
+
+/* Form Inputs  */
+/*component to display todo list items */
+/* useState {} to hold todos with {id: <todos.id>, name: <todos.name> status: <complete/incomplete>} */
+/* way to delete todo items as well as to strike through*/
