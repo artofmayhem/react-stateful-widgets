9ab1d25d30b5d49afa2fd784ac0eb1839d3a1457
Tony Miller <70006926+artofmayhem@users.noreply.github.com>
2021-01-19 02:00:46 -1000

    Update Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -57,7 +57,7 @@ const Todos = () => {
         className="d-flex flex-column justify-content-center
        align-content-center todo-page"
       >
-        <h3 style={{ padding: "2rem" }}>
+        <h5 style={{ padding: "2rem" }}>
           Lightbulb
         </h3>
         <h4 style={{marginBottom: ""}}>Inspiration Tracker</h4>
